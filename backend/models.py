bf8fa2bbf14148dd572f161878de2ef71997a9b1
Sameer Selokar <sameerselokar924@gmail.com>
2025-03-24 20:44:23 +0530

    update database URI to use friends.db; modify Friend model to remove unique constraint on name

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from app import db
 
 class Friend(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
     role = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text, nullable=False)
     gender = db.Column(db.String(10), nullable=False)
