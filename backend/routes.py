7a02360a6b5d6a87b40ab59bf315eb1e026db217
Sameer Selokar <sameerselokar924@gmail.com>
2025-03-26 18:53:35 +0530

    remove unnecessary blank lines in app and routes files

--- backend/routes.py
+++ backend/routes.py
@@ -2,7 +2,6 @@ from app import app, db
 from flask import request, jsonify
 from models import Friend
 
-
 @app.route("/api/friends", methods=["GET"])
 def get_friends():
     friends = Friend.query.all()
