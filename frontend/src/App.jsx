d12d84d3872e1fe0373f5114e030d4f591a10ac2
Sameer Selokar <sameerselokar924@gmail.com>
2025-03-29 10:28:11 +0530

    refactor: clean up code formatting and improve consistency in Fruit components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Fruits from './components/Fruis'
-import FruitList from './components/Fruis'
+import "./App.css";
+
+import FruitList from "./components/Fruis";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className="App">
         <header className="App-header">
           <h1>Fruit Management System</h1>
-
         </header>
         <main>
-          <FruitList/>
+          <FruitList />
         </main>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
